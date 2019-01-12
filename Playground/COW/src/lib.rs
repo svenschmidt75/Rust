143236c28dc239f7c808de5d903b232f97110a73
Sven Schmidt <svenschmidt75@googlemail.com>
2019-01-12 07:41:49 -0700

    trying to better understand lifetimes

--- Playground/COW/src/lib.rs
+++ Playground/COW/src/lib.rs
@@ -1,21 +1,23 @@
 use std::ops::Deref;
+use std::fmt::Debug;
+use std::fmt::Formatter;
+use std::fmt::Error;
 
 #[allow(dead_code)]
-enum COW<'a, T: ?Sized> {
-    Borrowed(&'a T),
-    Owned(&'a T)
+enum COW<'a> {
+    Borrowed(&'a str),
+    Owned(String)
 }
 
 #[allow(dead_code)]
-impl<'a, T: ?Sized> COW<'a, T> {
+impl<'a> COW<'a> {
 
-    pub fn to_mut(&'a mut self) -> &'a mut T {
+    pub fn to_mut(&mut self) -> &mut String {
         match *self {
             COW::Borrowed(borrowed) => {
-                *self = COW::Owned(borrowed.clone());
+                *self = COW::Owned(borrowed.to_string());
                 match *self {
                     COW::Owned(ref mut owned) => {
-                        let a: u8 = owned;
                         owned
                     },
                     COW::Borrowed(..) => unreachable!()
@@ -29,21 +31,49 @@ impl<'a, T: ?Sized> COW<'a, T> {
 
 }
 
-impl<'a, T: ?Sized + 'a> Deref for COW<'a, T> {
-    type Target = T;
+impl<'a> Deref for COW<'a> {
+    type Target = str;
 
     fn deref(&self) -> &<Self as Deref>::Target {
-        match self {
-            COW::Borrowed(val) => {
+        match *self {
+            COW::Borrowed(ref val) => {
                 val
             },
-            COW::Owned(val) => {
+            COW::Owned(ref val) => {
                 val
             }
         }
     }
-
 }
+
+impl<'a> PartialEq<COW<'a>> for &str {
+
+    fn eq(&self, other: &COW<'a>) -> bool {
+        match *other {
+            COW::Borrowed(ref val) => {
+                self == val
+            },
+            COW::Owned(ref val) => {
+                self == val
+            }
+        }
+    }
+}
+
+impl <'a> Debug for COW<'a> {
+
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            COW::Borrowed(ref val) => {
+                write!(f, "{}", val)
+            },
+            COW::Owned(ref val) => {
+                write!(f, "{}", val)
+            }
+        }
+    }
+}
+
 
 
 #[cfg(test)]
@@ -64,29 +94,17 @@ mod tests {
     fn deref_borrowed() {
         // Arrange
 
-        // Act
-        let cow = COW::Borrowed(&"test");
-
-        // Assert
-        assert_eq!("test", *cow);
-    }
-
-    #[test]
-    fn deref_to_mut_borrowed() {
-        // Arrange
-        let value = 1;
-        let mut cow: COW<i32> = COW::Borrowed(&value);
-        let mut owned_cow = cow.to_mut();
-
-        // Act
-//        owned_cow.push('2');
-//        String::push(&mut owned_cow, 'c');
-        owned_cow = &mut &2;
-
-        // Assert
-        assert_eq!(1, value);
-//        assert_eq!(&mut &2, owned_cow);
-    }
+        let mut cow = COW::Borrowed("test");                 // ---------+-- 'a
+        {                                                    //          |
+            let mut owned_cow: &mut String = cow.to_mut();   // -+-- 'b  |
+                                                             //  |       |
+            // Act                                           //  |       |
+            String::push(&mut owned_cow, 'c');  //  |       |
+        }                                                    // -|       |
+                                                             //         |
+        // Assert                                            //         |
+        assert_eq!("testc", cow);                            //         |
+    }                                                        // --------+
 
     #[test]
     fn deref_to_mut_borrowed3() {
@@ -118,7 +136,7 @@ mod tests {
         String::push(&mut owned_cow, 'c');
 
         // Assert
-        assert_eq!("test", value);
+        assert_eq!("testc", cow);
     }
 
 }
