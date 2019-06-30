b5dc04c062869eeb53846ab8b348450913b513da
Sven Schmidt <svenschmidt75@googlemail.com>
2019-06-30 08:10:51 -0600

    work on dropout layer

--- NeuralNetwork/lib/ann/src/ann/layers/dropout_layer.rs
+++ NeuralNetwork/lib/ann/src/ann/layers/dropout_layer.rs
@@ -2,24 +2,43 @@ use crate::ann::activation::Activation;
 use crate::ann::layers::layer::Layer;
 use linear_algebra::matrix::Matrix2D;
 use linear_algebra::vector::Vector;
+use rand::distributions::{Bernoulli, Distribution};
 
 pub struct DropoutLayer {
-    p: f64
+    p: f64,
+    probability_vector: Vector,
 }
 
 impl DropoutLayer {}
 
 impl Layer for DropoutLayer {
     fn initialize(&mut self, prev_layer: &Layer) {
-        unimplemented!()
+        // generate vector with p and 0
+        let distribution = Bernoulli::new(self.p);
+        let mut rng = rand::thread_rng();
+
+        let previous_activations = prev_layer.nactivations();
+        let data = (0..previous_activations)
+            .into_iter()
+            .map(|_| match distribution.sample(&mut rng) {
+                true => 1.0 / self.p,
+                _ => 0.0,
+            })
+            .collect::<Vec<_>>();
+
+        self.probability_vector = Vector::from(data);
     }
 
     fn feedforward(&self, a: &Vector) -> (Vector, Vector) {
-        unimplemented!()
+        let z = a.hadamard(&self.probability_vector);
+
+        // the activation function is the identity
+        let a2 =  a.clone();
+        (z, a2)
     }
 
     fn nactivations(&self) -> usize {
-        unimplemented!()
+        self.probability_vector.dim()
     }
 
     fn get_weights(&self) -> &Matrix2D {
