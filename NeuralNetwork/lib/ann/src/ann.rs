08bc28208f2c4b614e94c0432367f3b04e64be88
Sven Schmidt <svenschmidt75@googlemail.com>
2019-08-18 17:45:44 -0600

    move some tests to integration tests

--- NeuralNetwork/lib/ann/src/ann.rs
+++ NeuralNetwork/lib/ann/src/ann.rs
@@ -1,6 +1,6 @@
-mod activation;
-mod cost_function;
-mod layers;
-mod minibatch;
-mod model;
+pub mod activation;
+pub mod cost_function;
+pub mod layers;
+pub mod minibatch;
+pub mod model;
 pub mod training_data;
--- NeuralNetwork/lib/ann/src/lib.rs
+++ NeuralNetwork/lib/ann/src/lib.rs
@@ -1,10 +1,2 @@
-mod ann;
-mod la;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod ann;
+pub mod la;
