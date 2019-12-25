01d4bdce596446a1251f3600fd993c767b3e4532
Sven Schmidt <svenschmidt75@googlemail.com>
2019-12-25 09:19:34 -0700

    fix removal of elements

--- HeapSort/src/lib.rs
+++ HeapSort/src/lib.rs
@@ -1,12 +1,10 @@
-
 struct MinHeap {
-    heap: Vec<f64>
+    heap: Vec<f64>,
 }
 
 impl MinHeap {
-
     fn new(data: &[f64]) -> MinHeap {
-        let mut heap = MinHeap {heap: vec![]};
+        let mut heap = MinHeap { heap: vec![] };
         for i in data {
             heap.insert(*i);
         }
@@ -14,8 +12,63 @@ impl MinHeap {
     }
 
     fn insert(&mut self, v: f64) {
+        // SS: add new element at the end of heap
         self.heap.push(v);
         self.up_heapify(self.heap.len() - 1);
+    }
+
+    fn find(&self, value: f64) -> Option<usize> {
+        self.find_internal(0, value)
+    }
+
+    fn find_internal(&self, node_index: usize, value: f64) -> Option<usize> {
+        // SS: pre-order traversal
+        if node_index >= self.heap.len() {
+            return None;
+        }
+        if self.heap[node_index] == value {
+            Some(node_index)
+        } else {
+            // SS: min heap, so if smaller than current node, cannot be in tree
+            if value < self.heap[node_index] {
+                return None;
+            }
+            if let Some(left_child) = self.get_left_child(node_index) {
+                if let Some(v) = self.find_internal(left_child, value) {
+                    return Some(v);
+                }
+            };
+
+            if let Some(right_child) = self.get_right_child(node_index) {
+                if let Some(v) = self.find_internal(right_child, value) {
+                    return Some(v);
+                }
+            };
+
+            None
+        }
+    }
+
+    fn delete(&mut self, node_index: usize) {
+        // SS: swap node with last one
+        self.heap[node_index] = self.heap[self.heap.len() - 1];
+
+        // SS: delete element
+        self.heap.pop();
+
+        self.down_heapify(node_index);
+    }
+
+    fn swap(&mut self, n1: usize, n2: usize) {
+        if n1 >= self.heap.len() {
+            panic!("Node does not exist");
+        }
+        if n2 >= self.heap.len() {
+            panic!("Node does not exist");
+        }
+        let tmp = self.heap[n1];
+        self.heap[n1] = self.heap[n2];
+        self.heap[n2] = tmp;
     }
 
     fn up_heapify(&mut self, node_index: usize) {
@@ -37,92 +90,29 @@ impl MinHeap {
             return;
         }
 
-        let left_child = MinHeap::get_left_child(node_index);
-        if self.heap[left_child] < self.heap[node_index] {
-            self.swap(left_child, node_index);
-            self.down_heapify(left_child);
-        }
-
-        let right_child = MinHeap::get_right_child(node_index);
-        if self.heap[right_child] < self.heap[node_index] {
-            self.swap(right_child, node_index);
-            self.down_heapify(right_child);
-        }
-    }
-
-    fn find(&self, value: f64) -> Option<usize> {
-        self.find_internal(0, value)
-    }
-
-    fn find_internal(&self, node_index: usize, value: f64) -> Option<usize> {
-        // SS: pre-order traversal
-        if node_index >= self.heap.len() {
-            return None;
-        }
-        if self.heap[node_index] == value {
-            Some(node_index)
-        } else {
-            // SS: min heap, so if smaller than current node, cannot be in tree
-            if value < self.heap[node_index] {
-                return None;
-            }
-            let left_child = MinHeap::get_left_child(node_index);
-            if let Some(v) = self.find_internal(left_child, value) {
-                return Some(v);
-            }
-            let right_child = MinHeap::get_right_child(node_index);
-            if let Some(v) = self.find_internal(right_child, value) {
-                return Some(v);
-            }
-            None
-        }
-    }
-
-    fn delete(&mut self, node_index: usize) {
-        self.delete_internal(node_index);
-        self.down_heapify(node_index);
-    }
-
-    fn delete_internal(&mut self, node_index: usize) {
-        if node_index >= self.heap.len() {
-            panic!("Node does not exist");
-        }
-        if node_index == self.heap.len() -1 {
-            // SS: last element, remove
-            self.heap.pop();
-        } else {
-            let right_child = MinHeap::get_right_child(node_index);
-            if right_child >= self.heap.len() {
-                return;
+        if let Some(left_child) = self.get_left_child(node_index) {
+            if let Some(right_child) = self.get_right_child(node_index) {
+                // SS: swap with smaller child, so the smallest element ends up at the top
+                if self.heap[left_child] < self.heap[right_child] {
+                    // SS: swap with left child
+                    if self.heap[left_child] < self.heap[node_index] {
+                        self.swap(left_child, node_index);
+                        self.down_heapify(left_child);
+                    }
+                } else {
+                    if self.heap[right_child] < self.heap[node_index] {
+                        self.swap(right_child, node_index);
+                        self.down_heapify(right_child);
+                    }
+                }
             } else {
-                // SS: swap
-                self.swap(node_index, right_child);
-                self.delete_internal(right_child);
-                return;
+                // SS: no right child, so swap with left
+                if self.heap[left_child] < self.heap[node_index] {
+                    self.swap(left_child, node_index);
+                    self.down_heapify(left_child);
+                }
             }
-
-            let left_child = MinHeap::get_left_child(node_index);
-            if left_child >= self.heap.len() {
-                return;
-            } else {
-                // SS: swap
-                self.swap(node_index, left_child);
-                self.delete_internal(left_child);
-                return;
-            }
-        }
-    }
-
-    fn swap(&mut self, n1: usize, n2: usize) {
-        if n1 >= self.heap.len() {
-            panic!("Node does not exist");
-        }
-        if n2 >= self.heap.len() {
-            panic!("Node does not exist");
-        }
-        let tmp = self.heap[n1];
-        self.heap[n1] = self.heap[n2];
-        self.heap[n2] = tmp;
+        };
     }
 
     fn get_parent(node_index: usize) -> usize {
@@ -132,12 +122,22 @@ impl MinHeap {
         (node_index - 1) / 2
     }
 
-    fn get_left_child(node_index: usize) -> usize {
-        2 * node_index + 1
+    fn get_left_child(&self, node_index: usize) -> Option<usize> {
+        let child_index = 2 * node_index + 1;
+        if child_index < self.heap.len() {
+            Some(child_index)
+        } else {
+            None
+        }
     }
 
-    fn get_right_child(node_index: usize) -> usize {
-        2 * (node_index + 1)
+    fn get_right_child(&self, node_index: usize) -> Option<usize> {
+        let child_index = 2 * (node_index + 1);
+        if child_index < self.heap.len() {
+            Some(child_index)
+        } else {
+            None
+        }
     }
 
     fn is_minheap(&self) -> bool {
@@ -146,32 +146,38 @@ impl MinHeap {
 
     fn is_minheap_internal(&self, node_index: usize) -> bool {
         // SS: traverse tree in level order
-        let left_child = MinHeap::get_left_child(node_index);
-        if left_child >= self.heap.len() {
-            return true;
-        }
-        if self.heap[left_child] < self.heap[node_index] {
-            return false;
-        }
+        if let Some(left_child) = self.get_left_child(node_index) {
+            if left_child >= self.heap.len() {
+                return true;
+            }
 
-        let right_child = MinHeap::get_right_child(node_index);
-        if right_child >= self.heap.len() {
-            return true;
-        }
-        if self.heap[right_child] < self.heap[node_index] {
-            return false;
-        }
+            if self.heap[left_child] < self.heap[node_index] {
+                return false;
+            }
 
-        self.is_minheap_internal(left_child) && self.is_minheap_internal(right_child)
+            if let Some(right_child) = self.get_right_child(node_index) {
+                if right_child >= self.heap.len() {
+                    return true;
+                }
+
+                if self.heap[right_child] < self.heap[node_index] {
+                    return false;
+                }
+
+                return self.is_minheap_internal(left_child) && self.is_minheap_internal(right_child);
+            };
+
+            return self.is_minheap_internal(left_child);
+        };
+        true
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::MinHeap;
+    use super::*;
 
-    # [test]
+    #[test]
     fn test_create() {
         // Arrange
         let data = [38.0, 27.0, 43.0, 3.0, 9.0, 82.0, 10.0];
@@ -183,7 +189,7 @@ mod tests {
         assert!(heap.is_minheap())
     }
 
-    # [test]
+    #[test]
     fn test_find_1() {
         // Arrange
         let data = [38.0, 27.0, 43.0, 3.0, 9.0, 82.0, 10.0];
@@ -196,7 +202,7 @@ mod tests {
         assert!(found.is_some())
     }
 
-    # [test]
+    #[test]
     fn test_find_2() {
         // Arrange
         let data = [38.0, 27.0, 43.0, 3.0, 9.0, 82.0, 10.0];
@@ -209,7 +215,7 @@ mod tests {
         assert!(found.is_some())
     }
 
-    # [test]
+    #[test]
     fn test_find_3() {
         // Arrange
         let data = [38.0, 27.0, 43.0, 3.0, 9.0, 82.0, 10.0];
@@ -222,7 +228,7 @@ mod tests {
         assert!(found.is_none())
     }
 
-    # [test]
+    #[test]
     fn test_delete_root() {
         // Arrange
         let data = [38.0, 27.0, 43.0, 3.0, 9.0, 82.0, 10.0];
@@ -234,5 +240,4 @@ mod tests {
         // Assert
         assert!(heap.is_minheap())
     }
-
 }
