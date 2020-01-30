2ce6a6537a6ab0832c59bf1b1e49454ec42393f1
Sven Schmidt <svenschmidt75@googlemail.com>
2020-01-29 19:25:39 -0700

    another helper method

--- DSandAlg/RadixSort/src/lib.rs
+++ DSandAlg/RadixSort/src/lib.rs
@@ -5,12 +5,50 @@ fn get_digit(num: i64, pos: usize) -> i64 {
 
     let p2 = 10i64.pow(pos as u32);
     let digit = t2 / p2;
-    digit
+    digit.abs()
+}
+
+fn number_of_digits(num: i64) -> i64 {
+    let mut cnt = 0;
+    let mut base = 1;
+    loop {
+        let d = num / base;
+        if d == 0 {
+            break;
+        }
+        cnt += 1;
+        base *= 10;
+    }
+    cnt
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_digit;
+    use super::*;
+
+    #[test]
+    fn test_number_of_digits1() {
+        // Arrange
+        let number = 94671;
+
+        // Act
+        let digits = number_of_digits(number);
+
+        // Assert
+        assert_eq!(5, digits);
+    }
+
+    #[test]
+    fn test_get_digit_negative() {
+        // Arrange
+        let number = -94671;
+
+        // Act
+        let digit = get_digit(number, 2);
+
+        // Assert
+        assert_eq!(6, digit);
+    }
 
     #[test]
     fn test_get_digit1() {
