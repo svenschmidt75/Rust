d7fc16eeaba1ab25b68e3c69cd4748244a0fb93e
Sven Schmidt <svenschmidt75@googlemail.com>
2020-02-01 07:30:50 -0700

    add some comments

--- DSandAlg/CodingProblems/Knapsack/src/lib.rs
+++ DSandAlg/CodingProblems/Knapsack/src/lib.rs
@@ -5,10 +5,18 @@ fn grid_index(row: usize, col: usize, row_size: usize) -> usize {
 }
 
 fn knapsack_1(input: &[(usize, i64)], size: usize) -> i64 {
+    // SS: We don't actually need to generate and keep the entire grid around since
+    // we only ever look at the previous row, which is the optimum up until that
+    // item. We could use two "pointers", that we change accordingly when we move
+    // from the item to the next, such that the one points to the previous row,
+    // the other to the current.
     let mut grid = vec![0; input.len() * size];
 
+    // SS: each row corresponds to a new item that is available for stealing
     for item in 0..input.len() {
-        // SS: check whether to steal the current item or not
+
+        // SS: solve the problem for all knapsacks until the specified size
+        // These are the subproblems we solve
         for knapsack_size in 1..=size {
             let mut new_max = 0;
 
@@ -18,13 +26,16 @@ fn knapsack_1(input: &[(usize, i64)], size: usize) -> i64 {
                 new_max = input[item].1;
             }
 
-            // SS: take the optimal case from previous sub problem and SAME sub knapsack size
+            // SS: take the previous optimal case for the remaining knapsack size
+            // after subtracting the current item from the knacksack size
             if item > 0 && knapsack_size > input[item].0 {
                 let prev_col = size - input[item].0 - 1;
                 let prev_optimum = grid[grid_index(item - 1, prev_col, size)];
                 new_max += prev_optimum;
             }
 
+            // SS: check whether the previous optimum for the same knapsack size is better
+            // than stealing the current item
             let prev_size_optimum = if item > 0 {
                 grid[grid_index(item - 1, knapsack_size - 1, size)]
             } else {
