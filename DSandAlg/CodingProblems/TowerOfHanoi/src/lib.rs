b5f0921fef0fb524d5789247d45f2aac4e333618
Sven Schmidt <svenschmidt75@googlemail.com>
2020-02-23 09:38:07 -0700

    add benchmark

--- DSandAlg/CodingProblems/TowerOfHanoi/src/lib.rs
+++ DSandAlg/CodingProblems/TowerOfHanoi/src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 use std::collections::VecDeque;
 
 // SS: we are using stacks for the three pegs
@@ -27,6 +31,7 @@ fn hanoi(n: u8, from: &mut VecDeque<u8>, to: &mut VecDeque<u8>, tmp: &mut VecDeq
 mod tests {
     use crate::hanoi;
     use std::collections::VecDeque;
+    use test::Bencher;
 
     #[test]
     fn it_works() {
@@ -50,5 +55,24 @@ mod tests {
             let disc = to.pop_back().unwrap();
             assert_eq!(disc, i + 1);
         }
+    }
+
+    #[bench]
+    fn bench_add_two(b: &mut Bencher) {
+        b.iter(|| {
+            let n = 10;
+
+            let mut from = VecDeque::new();
+            let mut to = VecDeque::new();
+            let mut tmp = VecDeque::new();
+
+            // SS: insert discs
+            for i in 0..n {
+                from.push_back(n - i);
+            }
+
+            // Act
+            hanoi(n, &mut from, &mut to, &mut tmp);
+        });
     }
 }
