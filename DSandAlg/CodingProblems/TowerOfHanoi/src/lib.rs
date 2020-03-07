6c04561e0f8b65ea1341c31ad84d8742dfc2e39c
Sven Schmidt <svenschmidt75@googlemail.com>
2020-03-07 09:18:24 -0700

    print steps for Tower of Hanoi

--- DSandAlg/CodingProblems/TowerOfHanoi/src/lib.rs
+++ DSandAlg/CodingProblems/TowerOfHanoi/src/lib.rs
@@ -7,11 +7,11 @@ use std::collections::VecDeque;
 // SS: we are using stacks for the three pegs
 
 fn hanoi(n: u8, from: &mut VecDeque<u8>, to: &mut VecDeque<u8>, tmp: &mut VecDeque<u8>) {
-    // SS: runtime complexity id O(2^n)
+    // SS: runtime complexity is O(2^n)
 
     // SS: base case
     if n == 1 {
-        // SS: move disc from 'to' to 'to'
+        // SS: move disc from 'from' to 'to'
         let disc = from.pop_back().unwrap();
         to.push_back(disc);
     } else {
@@ -27,9 +27,26 @@ fn hanoi(n: u8, from: &mut VecDeque<u8>, to: &mut VecDeque<u8>, tmp: &mut VecDeq
     }
 }
 
+fn hanoi_with_steps(n: u8, from: &str, to: &str, tmp: &str) {
+    // SS: base case
+    if n == 1 {
+        // SS: move disc from 'from' to 'to'
+        println!("moving disc from {} to {}", from, to);
+    } else {
+        // SS: move tower n-1 from 'from' to 'tmp', using 'to' as temp
+        hanoi_with_steps(n - 1, from, tmp, to);
+
+        // SS: move disc from 'from' to 'to'
+        println!("moving disc from {} to {}", from, to);
+
+        // SS: move tower n-1 from 'tmp' to 'to', using 'from' as temp
+        hanoi_with_steps(n - 1, tmp, to, from);
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::hanoi;
+    use super::*;
     use std::collections::VecDeque;
     use test::Bencher;
 
@@ -74,5 +91,16 @@ mod tests {
             // Act
             hanoi(n, &mut from, &mut to, &mut tmp);
         });
+    }
+
+    #[test]
+    fn hanoi_with_steps_test() {
+        // Arrange
+        let n = 3;
+
+        // Act
+        hanoi_with_steps(n, "A", "C", "B");
+
+        // Assert
     }
 }
