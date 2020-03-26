0d3e12aec1e0524758158a83c3a7d09e09f21a97
Sven Schmidt <svenschmidt75@googlemail.com>
2020-03-26 07:36:03 -0600

    find permutation of smaller string in larger string

---
+++ DSandAlg/LookupPermutationStringInAnotherString/src/lib.rs
@@ -0,0 +1,75 @@
+// Cracking the Coding Interview,
+// 6th ed., page 70
+
+use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
+
+fn find_permuations<'a>(s: &str, b: &'a str) -> Vec<&'a str> {
+    assert!(s.len() < b.len());
+
+    // SS: find all permutations os smaller string s in string b
+
+    // SS: rather than generating all permutations of s and then finding them in b,
+    // we march through b with a window of size s and check whether all characters
+    // are a permutation of s.
+    let mut hash = HashMap::new();
+    s.chars().for_each(|c| match hash.get_mut(&c) {
+        None => {
+            hash.insert(c, 1);
+        }
+        Some(v) => {
+            *v += 1;
+        }
+    });
+
+    let mut results = vec![];
+
+    // SS: runtime: O(b * s), memory O(s)
+    let bc: Vec<char> = b.chars().collect();
+    for i in 0..(bc.len() - s.len() + 1) {
+        // SS: check up to the next s.len() chars in b...
+        let substr = &b[i..(i + s.len())];
+        if is_permuation(substr, &hash) {
+            results.push(substr);
+        }
+    }
+
+    results
+}
+
+fn is_permuation(a: &str, frequency_map: &HashMap<char, u32>) -> bool {
+    let mut current_hash = frequency_map.clone();
+    for c in a.chars() {
+        match current_hash.get_mut(&c) {
+            None => {
+                return false;
+            }
+            Some(entry) => {
+                if *entry == 0 {
+                    return false;
+                }
+                *entry -= 1;
+            }
+        };
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::find_permuations;
+
+    #[test]
+    fn test1() {
+        // Arrange
+        let s = "abbc";
+        let b = "cbabadcbbabbcbabaabccbabc";
+
+        // Act
+        let results = find_permuations(s, b);
+
+        // Act
+        assert_eq!(results.len(), 7);
+    }
+}
--- DSandAlg/PairsWithDistanceK/src/lib.rs
+++ DSandAlg/PairsWithDistanceK/src/lib.rs
@@ -1,14 +1,16 @@
 // Cracking the Coding Interview
 // 6th ed, page 67
 
-fn pairs_with_distance_k(input: &[u32], k: u32) -> Vec<(u32, u32)> {
+use std::collections::HashSet;
+
+fn pairs_with_distance_k_1(input: &[u32], k: u32) -> Vec<(u32, u32)> {
     // SS: sort array at O(n log n)
     let mut sorted_input = input.to_vec();
     sorted_input.sort();
 
     let mut result = vec![];
 
-    // SS: O(n)
+    // SS: this loop is O(n)
     let mut i = 0;
     let mut j = 1;
     while j < input.len() {
@@ -30,17 +32,49 @@ fn pairs_with_distance_k(input: &[u32], k: u32) -> Vec<(u32, u32)> {
     result
 }
 
+fn pairs_with_distance_k_2(input: &[u32], k: u32) -> Vec<(u32, u32)> {
+    // SS: use O(n) memory for faster lookup
+    let mut hash = HashSet::new();
+    input.iter().for_each(|&v| {
+        hash.insert(v);
+    });
+
+    let mut result = vec![];
+
+    // SS: this loop is O(n)
+    for &v1 in input {
+        let v2 = v1 + k;
+        if hash.contains(&v2) {
+            result.push((v1, v2));
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::pairs_with_distance_k;
+    use super::*;
 
     #[test]
-    fn it_works() {
+    fn test1() {
         // Arrange
         let input = [1, 7, 5, 9, 2, 12, 3];
 
         // Act
-        let pairs = pairs_with_distance_k(&input, 2);
+        let pairs = pairs_with_distance_k_1(&input, 2);
+
+        // Assert
+        assert_eq!(pairs.len(), 4);
+    }
+
+    #[test]
+    fn test2() {
+        // Arrange
+        let input = [1, 7, 5, 9, 2, 12, 3];
+
+        // Act
+        let pairs = pairs_with_distance_k_1(&input, 2);
 
         // Assert
         assert_eq!(pairs.len(), 4);
