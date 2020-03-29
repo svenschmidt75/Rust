1c1b1158802da45f820d122b3cc9b8d7656d88e2
Sven Schmidt <svenschmidt75@googlemail.com>
2020-03-29 11:51:20 -0600

    add Dijkstra computerphile graph example

--- DSandAlg/CodingProblems/Dijkstra/src/graph.rs
+++ DSandAlg/CodingProblems/Dijkstra/src/graph.rs
@@ -58,10 +58,15 @@ impl Graph {
         while pq.is_empty() == false && visited.len() < self.adjacency_list.len() {
             let (priority, vertex) = pq.dequeue();
 
+            if vertex == to_vertex {
+                distance_shortest_path = priority;
+
+                // SS: we're done
+                break;
+            }
+
             if visited.contains(&vertex) == false {
                 visited.insert(vertex);
-
-                distance_shortest_path = priority;
 
                 // SS: check all of vertex's neighbors and update their distances
                 // from the start vertex, from_vertex
@@ -102,7 +107,9 @@ impl Graph {
 mod tests {
     use super::*;
 
-    fn create_graph() -> Graph {
+    fn create_graph_1() -> Graph {
+        // Udemy: JavaScript Algorithms and Data Structures Masterclass
+        // Created by Colt Steele
         let mut graph = Graph::new();
 
         graph.add_vertex(0); // A
@@ -136,10 +143,76 @@ mod tests {
         graph
     }
 
+    fn create_graph_2() -> Graph {
+        // Youtube: Dijkstra's Algorithm - Computerphile
+        let mut graph = Graph::new();
+
+        graph.add_vertex(0); // S
+        graph.add_vertex(1); // A
+        graph.add_vertex(2); // B
+        graph.add_vertex(3); // D
+        graph.add_vertex(4); // F
+        graph.add_vertex(5); // H
+        graph.add_vertex(6); // G
+        graph.add_vertex(7); // E
+        graph.add_vertex(8); // K
+        graph.add_vertex(9); // I
+        graph.add_vertex(10); // J
+        graph.add_vertex(11); // L
+        graph.add_vertex(12); // C
+
+        // (A, S, 7)
+        // (A, B, 3)
+        // (A, D, 4)
+        graph.add_undirected_edge(1, 0, 7);
+        graph.add_undirected_edge(1, 2, 3);
+        graph.add_undirected_edge(1, 3, 4);
+
+        // (B, D, 4)
+        // (B, H, 1)
+        // (B, S, 2)
+        graph.add_undirected_edge(2, 3, 4);
+        graph.add_undirected_edge(2, 5, 1);
+        graph.add_undirected_edge(2, 0, 2);
+
+        // (C, L, 2)
+        // (C, S, 3)
+        graph.add_undirected_edge(12, 11, 2);
+        graph.add_undirected_edge(12, 0, 3);
+
+        // (D, F, 5)
+        graph.add_undirected_edge(3, 4, 5);
+
+        // (E, G, 2)
+        // (E, K, 5)
+        graph.add_undirected_edge(7, 6, 2);
+        graph.add_undirected_edge(7, 8, 5);
+
+        // (F, H, 3)
+        graph.add_undirected_edge(4, 5, 3);
+
+        // (G, H, 2)
+        graph.add_undirected_edge(6, 5, 2);
+
+        // (I, J, 6)
+        // (I, K, 4)
+        // (I, L, 4)
+        graph.add_undirected_edge(9, 10, 6);
+        graph.add_undirected_edge(9, 8, 4);
+        graph.add_undirected_edge(9, 11, 4);
+
+        // (J, K, 4)
+        // (J, L, 4)
+        graph.add_undirected_edge(10, 8, 4);
+        graph.add_undirected_edge(10, 11, 4);
+
+        graph
+    }
+
     #[test]
-    fn shortest_path() {
+    fn shortest_path_1() {
         // Arrange
-        let g = create_graph();
+        let g = create_graph_1();
 
         // Act
         let (distance, shortest_path) = g.shortest_path(0, 4);
@@ -147,5 +220,18 @@ mod tests {
         // Assert
         assert_eq!(distance, 6);
         assert_eq!(shortest_path, vec![0, 2, 3, 5, 4]);
+    }
+
+    #[test]
+    fn shortest_path_2() {
+        // Arrange
+        let g = create_graph_2();
+
+        // Act
+        let (distance, shortest_path) = g.shortest_path(0, 7);
+
+        // Assert
+        assert_eq!(distance, 7);
+        assert_eq!(shortest_path, vec![0, 2, 5, 6, 7]);
     }
 }
