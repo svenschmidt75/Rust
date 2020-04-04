14da5b2d35dd2b4ef8548463f31d5cfc9335d8b9
Sven Schmidt <svenschmidt75@googlemail.com>
2020-04-04 09:48:09 -0600

    use the prefix_sum approach

--- DSandAlg/CodingProblems/Deian/GoogleProblem2/src/lib.rs
+++ DSandAlg/CodingProblems/Deian/GoogleProblem2/src/lib.rs
@@ -9,7 +9,7 @@
 // day 4: 9
 // day 5: 10
 
-fn find_minimum_plane_capacity_1(weights: &[usize], max_days: usize) -> usize {
+fn find_minimum_plane_capacity_brute_force(weights: &[usize], max_days: usize) -> usize {
     if weights.is_empty() {
         return 0;
     }
@@ -50,9 +50,71 @@ fn find_minimum_plane_capacity_1(weights: &[usize], max_days: usize) -> usize {
     }
 }
 
+fn find_minimum_plane_capacity_prefix_sum(weights: &[usize], max_days: usize) -> usize {
+    if weights.is_empty() {
+        return 0;
+    }
+
+    if max_days <= 0 {
+        return 0;
+    }
+
+    // SS: use prefix sum, i.e. sum the weights this way:
+    // 1,    2,     3,     4,      5,    6, ...
+    // 1,    3,     6,    10,     15,   21, ...
+
+    let prefix_sum = weights
+        .iter()
+        .scan(0, |state, &x| {
+            *state += x;
+            Some(*state)
+        })
+        .collect::<Vec<_>>();
+
+    let max_weight = *weights.iter().max().unwrap();
+
+    let mut current_capacity = max_weight;
+    loop {
+        // SS: find index of current_capacity in prefix_sum
+        let mut pos = find_closest_min(&prefix_sum, current_capacity) + 1;
+        let mut remaining_capacity = current_capacity;
+        for day in 1..max_days {
+            while pos < prefix_sum.len() {
+                let next = prefix_sum[pos] - prefix_sum[pos - 1];
+                if next <= remaining_capacity {
+                    remaining_capacity -= next;
+                    pos += 1;
+                } else {
+                    remaining_capacity = current_capacity;
+                    break;
+                }
+            }
+        }
+
+        if pos == prefix_sum.len() {
+            return current_capacity;
+        }
+
+        current_capacity += 1;
+    }
+}
+
+fn find_closest_min(input: &[usize], key: usize) -> usize {
+    // SS: we assume this element exists
+    let mut idx = 0;
+    while input[idx] < key {
+        idx += 1;
+    }
+    if input[idx] > key {
+        idx -= 1;
+    }
+
+    idx
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::find_minimum_plane_capacity_1;
+    use crate::{find_minimum_plane_capacity_brute_force, find_minimum_plane_capacity_prefix_sum};
 
     #[test]
     fn test_brute_force_1() {
@@ -60,7 +122,7 @@ mod tests {
         let weights = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
         // Act
-        let capacity = find_minimum_plane_capacity_1(&weights, 5);
+        let capacity = find_minimum_plane_capacity_brute_force(&weights, 5);
 
         // Assert
         assert_eq!(capacity, 15);
@@ -72,7 +134,31 @@ mod tests {
         let weights = [10, 1, 9, 2, 8, 3, 7, 4, 6, 5];
 
         // Act
-        let capacity = find_minimum_plane_capacity_1(&weights, 5);
+        let capacity = find_minimum_plane_capacity_brute_force(&weights, 5);
+
+        // Assert
+        assert_eq!(capacity, 11);
+    }
+
+    #[test]
+    fn test_dp_1() {
+        // Arrange
+        let weights = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        // Act
+        let capacity = find_minimum_plane_capacity_prefix_sum(&weights, 5);
+
+        // Assert
+        assert_eq!(capacity, 15);
+    }
+
+    #[test]
+    fn test_dp_2() {
+        // Arrange
+        let weights = [10, 1, 9, 2, 8, 3, 7, 4, 6, 5];
+
+        // Act
+        let capacity = find_minimum_plane_capacity_prefix_sum(&weights, 5);
 
         // Assert
         assert_eq!(capacity, 11);
