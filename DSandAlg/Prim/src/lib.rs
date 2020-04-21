484bd5ae6268db641e4709dd40206595c3f0fbb1
Sven Schmidt <svenschmidt75@googlemail.com>
2020-04-21 10:20:57 -0600

    implement Prim's algorithm

--- DSandAlg/Prim/src/lib.rs
+++ DSandAlg/Prim/src/lib.rs
@@ -1,7 +1,131 @@
+mod pq;
+
+use crate::pq::PriorityQueue;
+use std::collections::{HashMap, HashSet};
+
+struct Graph {
+    adjacency_list: HashMap<u64, Vec<(u64, i64)>>,
+}
+
+impl Graph {
+    fn new() -> Graph {
+        Graph {
+            adjacency_list: HashMap::new(),
+        }
+    }
+
+    fn add_vertex(&mut self, vertex: u64) {
+        let mut adj_list = self.adjacency_list.get_mut(&vertex);
+        if let None = adj_list {
+            self.adjacency_list.insert(vertex, vec![]);
+        }
+    }
+
+    fn add_undirected_edge(&mut self, from_vertex: u64, to_vertex: u64, weight: i64) {
+        self.add_directed_edge(from_vertex, to_vertex, weight);
+        self.add_directed_edge(to_vertex, from_vertex, weight);
+    }
+
+    fn add_directed_edge(&mut self, from_vertex: u64, to_vertex: u64, weight: i64) {
+        let mut adj_list = self.adjacency_list.get_mut(&from_vertex).unwrap();
+        adj_list.push((to_vertex, weight));
+    }
+
+    fn prim(&self, start_vertex: u64) -> (u64, Vec<(u64, u64)>) {
+        // SS: implementation of Prim's algorithm
+        let mut mst_cost = 0;
+        let mut mst = vec![];
+
+        let mut visited = HashSet::new();
+
+        let mut pq = PriorityQueue::new();
+
+        let mut current_vertex = start_vertex;
+
+        let number_of_vertices = self.adjacency_list.len();
+
+        loop {
+            visited.insert(current_vertex);
+
+            if number_of_vertices == visited.len() {
+                break;
+            }
+
+            let edges = self.adjacency_list.get(&current_vertex).unwrap();
+            for edge in edges {
+                let (target_vertex, weight) = *edge;
+                if visited.contains(&target_vertex) == false {
+                    pq.insert(weight, (current_vertex, edge.0, edge.1));
+                }
+            }
+
+            let (_, (vertex1, vertex2, weight)) = pq.pop();
+            mst.push((vertex1, vertex2));
+            mst_cost += weight;
+            current_vertex = vertex2;
+        }
+
+        (mst_cost as u64, mst)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    fn create_graph_1() -> Graph {
+        // Udemy - https://www.udemy.com/course/algorithms-and-data-structures-in-python/
+        // Prim's algorithm, section 15, module 111
+        let mut graph = Graph::new();
+
+        graph.add_vertex(0); // A
+        graph.add_vertex(1); // B
+        graph.add_vertex(2); // C
+        graph.add_vertex(3); // D
+        graph.add_vertex(4); // E
+        graph.add_vertex(5); // F
+        graph.add_vertex(6); // G
+
+        // (A, B, 2)
+        // (A, C, 6)
+        // (A, E, 5)
+        // (A, F, 10)
+        graph.add_undirected_edge(0, 1, 2);
+        graph.add_undirected_edge(0, 2, 6);
+        graph.add_undirected_edge(0, 4, 5);
+        graph.add_undirected_edge(0, 5, 10);
+
+        // (B, D, 3)
+        // (B, E, 3)
+        graph.add_undirected_edge(1, 3, 3);
+        graph.add_undirected_edge(1, 4, 3);
+
+        // (C, D, 1)
+        // (C, F, 2)
+        graph.add_undirected_edge(2, 3, 1);
+        graph.add_undirected_edge(2, 5, 2);
+
+        // (D, E, 4)
+        // (D, G, 5)
+        graph.add_undirected_edge(3, 4, 4);
+        graph.add_undirected_edge(3, 6, 5);
+
+        // (F, G, 3)
+        graph.add_undirected_edge(5, 6, 3);
+
+        graph
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn shortest_path_1() {
+        // Arrange
+        let g = create_graph_1();
+
+        // Act
+        let (mst_cost, mst) = g.prim(3);
+
+        // Assert
+        assert_eq!(mst_cost, 14);
+        assert_eq!(mst, vec![(3, 2), (2, 5), (3, 1), (1, 0), (1, 4), (5, 6)]);
     }
 }
---
+++ DSandAlg/Prim/src/pq.rs
@@ -0,0 +1,172 @@
+use std::{cmp, mem};
+
+struct MinHeap<K, V>
+where
+    K: Eq + PartialOrd + Copy,
+    V: Copy,
+{
+    heap: Vec<(K, V)>,
+}
+
+impl<K, V> MinHeap<K, V>
+where
+    K: Eq + PartialOrd + Copy,
+    V: Copy,
+{
+    fn new_from_data(data: &[(K, V)]) -> MinHeap<K, V> {
+        let mut heap = MinHeap {
+            heap: Vec::<(K, V)>::new(),
+        };
+        for &i in data {
+            heap.insert(i);
+        }
+        heap
+    }
+
+    fn new() -> MinHeap<K, V> {
+        MinHeap {
+            heap: Vec::<(K, V)>::new(),
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.heap.is_empty()
+    }
+
+    fn insert(&mut self, v: (K, V)) {
+        // SS: add new element at the end of heap
+        self.heap.push(v);
+        self.up_heapify(self.heap.len() - 1);
+    }
+
+    fn delete(&mut self, node_index: usize) -> Option<(K, V)> {
+        if self.heap.len() > 1 {
+            // SS: swap node with last one
+            let length = self.heap.len() - 1;
+            let (a, b) = self.heap.split_at_mut(length);
+            mem::swap(&mut a[node_index], &mut b[0]);
+        }
+
+        // SS: delete element
+        let element = self.heap.pop();
+
+        self.down_heapify(node_index);
+
+        element
+    }
+
+    fn swap(&mut self, n1: usize, n2: usize) {
+        if n1 >= self.heap.len() {
+            panic!("Node does not exist");
+        }
+        if n2 >= self.heap.len() {
+            panic!("Node does not exist");
+        }
+
+        let mut nlow = cmp::min(n1, n2);
+        let mut nhigh = cmp::max(n1, n2);
+        let (left, right) = self.heap.split_at_mut(nhigh);
+        mem::swap(&mut left[nlow], &mut right[0]);
+    }
+
+    fn up_heapify(&mut self, node_index: usize) {
+        if node_index == 0 {
+            // done
+            return;
+        }
+
+        let mut parent_node = MinHeap::<K, V>::get_parent(node_index);
+        if self.heap[parent_node].0 > self.heap[node_index].0 {
+            self.swap(parent_node, node_index);
+
+            // SS: heapify all the way to the top
+            self.up_heapify(parent_node);
+        }
+    }
+
+    fn down_heapify(&mut self, node_index: usize) {
+        if node_index >= self.heap.len() {
+            // done
+            return;
+        }
+
+        if let Some(left_child) = self.get_left_child(node_index) {
+            if let Some(right_child) = self.get_right_child(node_index) {
+                // SS: swap with smaller child, so the smallest element ends up at the top
+                if self.heap[left_child].0 < self.heap[right_child].0 {
+                    // SS: swap with left child
+                    if self.heap[left_child].0 < self.heap[node_index].0 {
+                        self.swap(left_child, node_index);
+                        self.down_heapify(left_child);
+                    }
+                } else {
+                    if self.heap[right_child].0 < self.heap[node_index].0 {
+                        self.swap(right_child, node_index);
+                        self.down_heapify(right_child);
+                    }
+                }
+            } else {
+                // SS: no right child, so swap with left
+                if self.heap[left_child].0 < self.heap[node_index].0 {
+                    self.swap(left_child, node_index);
+                    self.down_heapify(left_child);
+                }
+            }
+        };
+    }
+
+    fn get_parent(node_index: usize) -> usize {
+        if node_index == 0 {
+            panic!("root has no parent");
+        }
+        (node_index - 1) / 2
+    }
+
+    fn get_left_child(&self, node_index: usize) -> Option<usize> {
+        let child_index = 2 * node_index + 1;
+        if child_index < self.heap.len() {
+            Some(child_index)
+        } else {
+            None
+        }
+    }
+
+    fn get_right_child(&self, node_index: usize) -> Option<usize> {
+        let child_index = 2 * (node_index + 1);
+        if child_index < self.heap.len() {
+            Some(child_index)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct PriorityQueue<T>
+where
+    T: Copy,
+{
+    heap: MinHeap<i64, T>,
+}
+
+impl<T> PriorityQueue<T>
+where
+    T: Copy,
+{
+    pub fn new() -> PriorityQueue<T> {
+        PriorityQueue {
+            heap: MinHeap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, priority: i64, value: T) {
+        self.heap.insert((priority, value));
+    }
+
+    pub fn pop(&mut self) -> (i64, T) {
+        self.heap.delete(0).unwrap()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.heap.is_empty()
+    }
+}
