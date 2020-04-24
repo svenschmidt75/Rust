d2fa5b6d0b6f3b98b68d2c6b634548e5839bf86f
Sven Schmidt <svenschmidt75@googlemail.com>
2020-04-24 13:37:10 -0600

    add O(N)  solution

--- DSandAlg/CodingProblems/CrackingTheCodingInterview/10_11/src/lib.rs
+++ DSandAlg/CodingProblems/CrackingTheCodingInterview/10_11/src/lib.rs
@@ -1,13 +1,14 @@
 // Cracking the Coding Interview
 // 6th ed, p. 151, ex. 10.11
 
-fn peaks_and_valleys(input: &[u64]) -> Vec<u64> {
+fn peaks_and_valleys_1(input: &[u64]) -> Vec<u64> {
     // SS: O(N log N)
     let mut sorted = input.to_owned();
     sorted.sort();
 
     let mut result = vec![];
 
+    // SS: O(N)
     for i in 0..(sorted.len() / 2) {
         let value = sorted[sorted.len() - 1 - i];
         result.push(value);
@@ -23,31 +24,85 @@ fn peaks_and_valleys(input: &[u64]) -> Vec<u64> {
     result
 }
 
+fn peaks_and_valleys_2(input: &[u64]) -> Vec<u64> {
+    if input.len() < 3 {
+        input.to_vec()
+    } else {
+        let mut result = input.to_owned();
+
+        // SS: O(N) solution
+
+        let mut i = 0;
+        while i < (result.len() - 2) {
+            // SS: put the largest at index i
+            if result[i] < result[i + 1] {
+                result.swap(i, i + 1);
+            }
+            if result[i] < result[i + 2] {
+                result.swap(i, i + 2);
+            }
+
+            // SS: put the middle one at index i + 1
+            if result[i + 1] > result[i + 2] {
+                result.swap(i + 1, i + 2);
+            }
+
+            i += 2;
+        }
+
+        result
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test1() {
+    fn test11() {
         // Arrange
         let input = [5, 3, 1, 2, 3];
 
         // Act
-        let result = peaks_and_valleys(&input);
+        let result = peaks_and_valleys_1(&input);
 
         // Assert
         assert_eq!(result, vec![5, 1, 3, 2, 3]);
     }
 
     #[test]
-    fn test2() {
+    fn test12() {
         // Arrange
         let input = [5, 8, 6, 2, 3, 4, 6];
 
         // Act
-        let result = peaks_and_valleys(&input);
+        let result = peaks_and_valleys_1(&input);
 
         // Assert
         assert_eq!(result, vec![8, 2, 6, 3, 6, 4, 5]);
+    }
+
+    #[test]
+    fn test21() {
+        // Arrange
+        let input = [5, 3, 1, 2, 3];
+
+        // Act
+        let result = peaks_and_valleys_2(&input);
+
+        // Assert
+        assert_eq!(result, vec![5, 1, 3, 2, 3]);
+    }
+
+    #[test]
+    fn test22() {
+        // Arrange
+        let input = [5, 8, 6, 2, 3, 4, 6];
+
+        // Act
+        let result = peaks_and_valleys_2(&input);
+
+        // Assert
+        assert_eq!(result, vec![8, 5, 6, 2, 6, 3, 4]);
     }
 }
