83f346d21bffafc0ed02f4048044a6fd93d3eae1
Sven <svenschmidt75@googlemail.com>
2020-04-28 08:20:32 -0600

    add memoization to cut down runtime from O(3^{N}) to O(N)

--- DSandAlg/CodingProblems/TransformStringAIntoStringB/src/lib.rs
+++ DSandAlg/CodingProblems/TransformStringAIntoStringB/src/lib.rs
@@ -4,7 +4,10 @@
 
 use std::cmp;
 
-fn convert(s1: &str, s2: &str, i1: usize, i2: usize) -> usize {
+fn transform(s1: &str, s2: &str, i1: usize, i2: usize, buffer: &mut [Vec<usize>]) -> usize {
+    // SS: This Divide & Conquor solution has runtime complexity O(3^{N})!
+    // DP adds memoization to this, in order to reduce it to linear time O(N)!
+    // via buffer
     if i1 == s1.len() && i2 == s2.len() {
         // SS: both strings exhausted, nothing to do
         0
@@ -21,48 +24,54 @@ fn convert(s1: &str, s2: &str, i1: usize, i2: usize) -> usize {
 
         // SS: if both chars are the same, nothing to do
         if c1 == c2 {
-            convert(s1, s2, i1 + 1, i2 + 1)
+            transform(s1, s2, i1 + 1, i2 + 1, buffer)
         } else {
-            // SS: 3 options:
+            // SS: use cache?
+            if buffer[i1][i2] == 0 {
+                // SS: 3 options:
 
-            // 1. we replace the char in s2
-            // Example:
-            // s2 = a
-            // s1 = b
-            // i1 = i2 = 0
-            let a1 = 1 + convert(s1, s2, i1 + 1, i2 + 1);
+                // 1. we replace the char in s2
+                // Example:
+                // s2 = a
+                // s1 = b
+                // i1 = i2 = 0
+                let a1 = 1 + transform(s1, s2, i1 + 1, i2 + 1, buffer);
 
-            // 2. we remove the char in s2
-            // Example:
-            // s2 = ab
-            // s1 =  b
-            // i1 = i2 = 0
-            let a2 = 1 + convert(s1, s2, i1, i2 + 1);
+                // 2. we remove the char in s2
+                // Example:
+                // s2 = ab
+                // s1 =  b
+                // i1 = i2 = 0
+                let a2 = 1 + transform(s1, s2, i1, i2 + 1, buffer);
 
-            // 3. we insert the char into s2
-            // Example:
-            // s2 = b
-            // s1 = ab
-            // i1 = i2 = 0
-            let a3 = 1 + convert(s1, s2, i1 + 1, i2);
+                // 3. we insert the char into s2
+                // Example:
+                // s2 = b
+                // s1 = ab
+                // i1 = i2 = 0
+                let a3 = 1 + transform(s1, s2, i1 + 1, i2, buffer);
 
-            cmp::min(a1, cmp::min(a2, a3))
+                buffer[i1][i2] = cmp::min(a1, cmp::min(a2, a3));
+            }
+
+            buffer[i1][i2]
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::convert;
+    use crate::transform;
 
     #[test]
     fn test1() {
         // Arrange
         let s1 = "a";
         let s2 = "a";
+        let mut buffer = vec![vec![0_usize; 1]; 1];
 
         // Arrange
-        let operations = convert(s1, s2, 0, 0);
+        let operations = transform(s1, s2, 0, 0, &mut buffer);
 
         // Assert
         assert_eq!(operations, 0);
@@ -73,9 +82,10 @@ mod tests {
         // Arrange
         let s1 = "b";
         let s2 = "a";
+        let mut buffer = vec![vec![0_usize; 1]; 1];
 
         // Arrange
-        let operations = convert(s1, s2, 0, 0);
+        let operations = transform(s1, s2, 0, 0, &mut buffer);
 
         // Assert
         assert_eq!(operations, 1);
@@ -86,12 +96,26 @@ mod tests {
         // Arrange
         let s1 = "bb";
         let s2 = "a";
+        let mut buffer = vec![vec![0_usize; 2]; 2];
 
         // Arrange
-        let operations = convert(s1, s2, 0, 0);
+        let operations = transform(s1, s2, 0, 0, &mut buffer);
 
         // Assert
         assert_eq!(operations, 2);
     }
 
+    #[test]
+    fn test4() {
+        // Arrange
+        let s1 = "This is a       test";
+        let s2 = "This is another test";
+        let mut buffer = vec![vec![0_usize; s2.len()]; s1.len()];
+
+        // Arrange
+        let operations = transform(s1, s2, 0, 0, &mut buffer);
+
+        // Assert
+        assert_eq!(operations, 6);
+    }
 }
