879c4e0cbccaf718446e545b449f83383d6aa702
Sven <svenschmidt75@googlemail.com>
2020-05-14 08:06:46 -0600

    add runtime analysis annotations

--- DSandAlg/CodingProblems/Deian/GoogleProblem11/src/lib.rs
+++ DSandAlg/CodingProblems/Deian/GoogleProblem11/src/lib.rs
@@ -18,8 +18,9 @@ fn from_flat_index(index: usize, ncols: usize) -> (usize, usize) {
 fn solve_non_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
     // SS: solve without using graph
     // The pre-processing is a bit messy...
+    // N = nrows * ncols
 
-    // SS: pre-processing, O(nrows * ncols)
+    // SS: pre-processing, O(N)
     let mut marbles_by_row = HashMap::new();
     let mut marbles_by_col = HashMap::new();
 
@@ -52,7 +53,7 @@ fn solve_non_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
     let mut pq = PriorityQueue::new();
 
     // SS: push all marbles onto the min PQ
-    // SS: pre-processing, O(nrows * ncols)
+    // SS: pre-processing, O(N log N)
     for (row, col) in marbles {
         let mut interaction_number = 0;
         let &nr = marbles_by_row.get(&row).unwrap();
@@ -65,6 +66,7 @@ fn solve_non_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
             interaction_number += nc - 1;
         }
 
+        // SS: O(log N)
         pq.insert(interaction_number as i64, (row, col));
 
         let hash_key = (row, col);
@@ -79,12 +81,16 @@ fn solve_non_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
 
     let mut max_removed = 0;
 
+    // SS: O(N (log N + sqrt(N) log N)) = O( N^{3/2} log N )
     while pq.is_empty() == false {
+        // SS: O(log N)
         let (interaction_number, (row, col)) = pq.pop();
 
         // SS: decrease the interaction numbers of all other marbles in the same row/col
         // and remove the current one.
         let marbles = mbr.get_mut(&row).unwrap();
+
+        // SS: O(sqrt(N) log N), i.e. only marbles in row
         for key in marbles {
             let (other_row, other_col) = *key;
             let other_interaction_number = marbles_hash.get_mut(&key).unwrap();
@@ -95,12 +101,15 @@ fn solve_non_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
                 *other_interaction_number -= 1;
                 if *other_interaction_number > 0 {
                     // SS: rather than reinserting, can also update priority of existing element
+                    // For time complexity, we assume we update at cost O(log N)
                     pq.insert(*other_interaction_number as i64, (other_row, other_col));
                 }
             }
         }
 
         let marbles = mbc.get_mut(&col).unwrap();
+
+        // SS: O(sqrt(N) log N), i.e. only marbles in column
         for key in marbles {
             let (other_row, other_col) = *key;
             let other_interaction_number = marbles_hash.get_mut(&key).unwrap();
@@ -108,6 +117,7 @@ fn solve_non_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
                 *other_interaction_number -= 1;
                 if *other_interaction_number > 0 {
                     // SS: rather than reinserting, can also update priority of existing element
+                    // For time complexity, we assume we update at cost O(log N)
                     pq.insert(*other_interaction_number as i64, (other_row, other_col));
                 }
             }
@@ -115,6 +125,10 @@ fn solve_non_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
     }
 
     max_removed
+}
+
+fn solve_graph(grid: &[u8], nrows: usize, ncols: usize) -> u32 {
+    // SS: solve using graph
 }
 
 #[cfg(test)]
