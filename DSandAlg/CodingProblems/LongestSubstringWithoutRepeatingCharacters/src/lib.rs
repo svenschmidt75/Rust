117f06db5600053c9698ca54d4b90bbe33838c55
Sven <svenschmidt75@googlemail.com>
2020-05-18 17:41:51 -0600

    use sliding window for O(n) runtime

--- DSandAlg/CodingProblems/LongestSubstringWithoutRepeatingCharacters/src/lib.rs
+++ DSandAlg/CodingProblems/LongestSubstringWithoutRepeatingCharacters/src/lib.rs
@@ -16,7 +16,13 @@ fn set_character(c: char, characters: &mut u32) {
     *characters = *characters | bit;
 }
 
-fn solve(input: &str) -> u32 {
+fn clear_character(c: char, characters: &mut u32) {
+    let n = c as u8 - 'a' as u8;
+    let bit = 1 << n;
+    *characters = *characters & (!bit);
+}
+
+fn solve_slow(input: &str) -> u32 {
     // SS: runtime complexity: O(N^2), space: O(1)
     // assuming all chars are lower-case
     let mut max_length = 1;
@@ -52,53 +58,128 @@ fn solve(input: &str) -> u32 {
     max_length
 }
 
+fn solve_fast(input: &str) -> u32 {
+    // SS: use sliding window, runtime complexity: O(N), space: O(1)
+    // assuming all chars are lower-case
+    let mut max_length = 0;
+
+    let cs = input.chars().collect::<Vec<_>>();
+
+    let mut characters: u32 = 0;
+
+    let mut i = 0;
+    let mut j = 0;
+
+    while i < input.len() && j < input.len() {
+        let c = cs[j];
+        if find_character(c, characters) {
+            let c = cs[i];
+            clear_character(c, &mut characters);
+            i += 1;
+        } else {
+            set_character(c, &mut characters);
+            j += 1;
+            max_length = cmp::max(max_length, j - i);
+        }
+    }
+    max_length as u32
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::solve;
+    use super::*;
 
     #[test]
-    fn test1() {
+    fn test11() {
         // Arrange
         let input = "abcabcbb";
 
         // Act
-        let max_length = solve(input);
+        let max_length = solve_slow(input);
 
         // Assert
         assert_eq!(max_length, 3);
     }
 
     #[test]
-    fn test2() {
+    fn test12() {
+        // Arrange
+        let input = "abcabcbb";
+
+        // Act
+        let max_length = solve_fast(input);
+
+        // Assert
+        assert_eq!(max_length, 3);
+    }
+
+    #[test]
+    fn test21() {
         // Arrange
         let input = "bbbbb";
 
         // Act
-        let max_length = solve(input);
+        let max_length = solve_slow(input);
 
         // Assert
         assert_eq!(max_length, 1);
     }
 
     #[test]
-    fn test3() {
+    fn test22() {
+        // Arrange
+        let input = "bbbbb";
+
+        // Act
+        let max_length = solve_fast(input);
+
+        // Assert
+        assert_eq!(max_length, 1);
+    }
+
+    #[test]
+    fn test31() {
         // Arrange
         let input = "pwwkew";
 
         // Act
-        let max_length = solve(input);
+        let max_length = solve_slow(input);
 
         // Assert
         assert_eq!(max_length, 3);
     }
 
     #[test]
-    fn test4() {
+    fn test32() {
+        // Arrange
+        let input = "pwwkew";
+
+        // Act
+        let max_length = solve_fast(input);
+
+        // Assert
+        assert_eq!(max_length, 3);
+    }
+
+    #[test]
+    fn test41() {
         // Arrange
         let input = "abcade";
 
         // Act
-        let max_length = solve(input);
+        let max_length = solve_slow(input);
+
+        // Assert
+        assert_eq!(max_length, 5);
+    }
+
+    #[test]
+    fn test42() {
+        // Arrange
+        let input = "abcade";
+
+        // Act
+        let max_length = solve_fast(input);
 
         // Assert
         assert_eq!(max_length, 5);
