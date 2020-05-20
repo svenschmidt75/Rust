dc4d3075f721cf3a5bf6056c19fc349d38223812
Sven <svenschmidt75@googlemail.com>
2020-05-20 15:11:22 -0600

    Kadane's algorithm

---
+++ DSandAlg/CodingProblems/KadaneAlgorithm/src/lib.rs
@@ -0,0 +1,39 @@
+// Largest sum in subarray
+// Uses Kadane's algorithm, running at O(n)
+// Dynamic programing technique
+
+use std::cmp;
+
+fn solve(input: &[i32]) -> i32 {
+    if input.len() < 2 {
+        input[0]
+    } else {
+        let mut largest_sum = input[0];
+        let mut sum = largest_sum;
+
+        for i in 1..input.len() {
+            let item = input[i];
+            sum = cmp::max(item, sum + item);
+            largest_sum = cmp::max(largest_sum, sum);
+        }
+
+        largest_sum
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        // Arrange
+        let input = [1, -3, 2, 1, -1];
+
+        // Act
+        let sum = solve(&input);
+
+        // Assert
+        assert_eq!(sum, 3);
+    }
+}
