fef1a3bf8a805e7637302a10c1ae4ca77cf7d013
Sven Schmidt <svenschmidt75@googlemail.com>
2026-02-11 06:25:56 -0700

    implement camera transform

--- software_3d_renderer/src/camera.rs
+++ software_3d_renderer/src/camera.rs
@@ -13,27 +13,46 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(location: Vertex4, direction: Vertex4, up: Vertex4) -> Self {
+    pub fn new(position: Vertex4, direction: Vertex4, up: Vertex4) -> Self {
+        // SS: compute right-handed coordinates
         let w = -direction.normalized();
         let u = cross_product(up, w).normalized();
         let v = cross_product(w, u);
 
-        
+        // SS: compute camera matrix transform
+        let mut m1 = Matrix4::identity();
+        m1[0][3] = - position[0];
+        m1[1][3] = - position[1];
+        m1[2][3] = - position[2];
+
+        let mut m2 = Matrix4::identity();
+        m2[0][0] = u[0];
+        m2[0][1] = u[1];
+        m2[0][2] = u[2];
+
+        m2[1][0] = v[0];
+        m2[1][1] = v[1];
+        m2[1][2] = v[2];
+
+        m2[2][0] = w[0];
+        m2[2][1] = w[1];
+        m2[2][2] = w[2];
+
+        let camera_matrix = m2 * m1;
 
         Self {
-            location,
+            location: position,
             direction,
             up,
             w,
             u,
             v,
+            matrix: camera_matrix,
         }
     }
 
     pub fn world_to_camera(&self, vertex: Vertex4) -> Vertex4 {
-        // SS: translate
-
-        // SS: rotate
-        vertex
+        // SS: transform vertex from world to camera space
+        self.matrix * vertex
     }
 }
--- software_3d_renderer/src/matrix4.rs
+++ software_3d_renderer/src/matrix4.rs
@@ -1,4 +1,5 @@
-use std::ops::Index;
+use crate::vertex::Vertex4;
+use std::ops::{Index, IndexMut, Mul};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Matrix4 {
@@ -6,13 +7,13 @@ pub struct Matrix4 {
 }
 
 impl Matrix4 {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             data: [[0.0; 4]; 4],
         }
     }
 
-    fn identity() -> Self {
+    pub fn identity() -> Self {
         Self {
             data: [
                 [1.0, 0.0, 0.0, 0.0],
@@ -29,5 +30,42 @@ impl Index<usize> for Matrix4 {
 
     fn index(&self, row: usize) -> &Self::Output {
         &self.data[row]
+    }
+}
+
+impl IndexMut<usize> for Matrix4 {
+    fn index_mut(&mut self, row: usize) -> &mut Self::Output {
+        &mut self.data[row]
+    }
+}
+
+impl Mul for Matrix4 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut result = Self::new();
+
+        // SS: perform matrix multiplication
+        for i in 0..4 {
+            for j in 0..4 {
+                for k in 0..4 {
+                    result[i][j] += self[i][k] * rhs[k][j];
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl Mul<Vertex4> for Matrix4 {
+    type Output = Vertex4;
+
+    fn mul(self, rhs: Vertex4) -> Self::Output {
+        Vertex4::new_vector(
+            self[0][0] * rhs[0] + self[0][1] * rhs[1] + self[0][2] * rhs[2],
+            self[1][0] * rhs[0] + self[1][1] * rhs[1] + self[1][2] * rhs[2],
+            self[2][0] * rhs[0] + self[2][1] * rhs[1] + self[2][2] * rhs[2],
+        )
     }
 }
